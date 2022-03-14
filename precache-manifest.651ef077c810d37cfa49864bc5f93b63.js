e34c75068495d54da2acc4855296027c54203e9c
ch tanveer <83070239+chTanveer@users.noreply.github.com>
2022-03-14 23:59:49 +0500

    Add files via upload

---
+++ precache-manifest.651ef077c810d37cfa49864bc5f93b63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b40001e5971c92d8b14c32360ea321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8601d291c995716cca12",
+    "url": "/static/css/main.5e263d78.chunk.css"
+  },
+  {
+    "revision": "a386649cb215431c0c93",
+    "url": "/static/js/2.633b5054.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.633b5054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8601d291c995716cca12",
+    "url": "/static/js/main.c5a21431.chunk.js"
+  },
+  {
+    "revision": "5cb44c3aab0f3a5be95a",
+    "url": "/static/js/runtime-main.574d4e1c.js"
+  },
+  {
+    "revision": "64a2da37514bf7f2549f88db609cb9f3",
+    "url": "/static/media/tanveer.64a2da37.jpg"
+  },
+  {
+    "revision": "45f4e6b213a91573bf0052da3b75c51d",
+    "url": "/static/media/technical.45f4e6b2.png"
+  },
+  {
+    "revision": "4e2fb141e22ae823775e14ac5d3662e5",
+    "url": "/static/media/technical2.4e2fb141.jpg"
+  }
+]);
